ff3c0c207799ea099b8204559387b505450350d3
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2022-03-28 15:27:51 -0300

    Tarea Clase7

--- js/pruebas.js
+++ js/pruebas.js
@@ -9,6 +9,31 @@ function probarValidarNombre() {
       "Este campo debe tener menos de 50 caracteres",
     "validarNombre no valido que el nombre sea menor a 50 caracteres"
   );
+
+  console.assert(
+    validarNombre("Fabricio") === "",
+    "validarnombre falló con un nombre correcto"
+  );
 }
 
 probarValidarNombre();
+
+function probarValidarCiudad() {
+  console.assert(
+    validarCiudad("Buenos Aires") === "",
+    "validarCiudad no funciono con un nombre de ciudad válido"
+  );
+  console.assert(
+    validarCiudad(" ") === "El campo ciudad no puede estar vacio",
+    "validarCiudad no mostró un error cuando la ciudad es vacía"
+  );
+}
+
+probarValidarCiudad();
+
+function probarValidarDescripcionRegalo() {
+  console.assert(
+    validarDescripcionRegalo("") === "El campo no puede estar vacio",
+    "validarDescripcionRegalo() no funciona si esta vacío"
+  );
+}
