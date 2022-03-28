ef670f56b63088ab5d8a99164e4f1fd56603e34f
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2022-03-28 14:41:18 -0300

    Siguiendo clase
    Nada importante

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,14 +1,13 @@
 function probarValidarNombre() {
   console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
+    validarNombre("") === "Este campo debe tener al menos 1 caracter",
+    "validarNombre() no funciono con un string vacio"
   );
 
   console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+    validarNombre("111111111111111111111111111111111111111111111111111") ===
+      "Este campo debe tener menos de 50 caracteres",
+    "validarNombre no valido que el nombre sea menor a 50 caracteres"
   );
 }
 
