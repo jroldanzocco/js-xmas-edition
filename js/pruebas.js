e138a215bb9008842ecea49d2aebbd114785d0d9
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2022-03-29 23:10:19 -0300

    Correccion tarea 7

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,39 +1,20 @@
 function probarValidarNombre() {
   console.assert(
     validarNombre("") === "Este campo debe tener al menos 1 caracter",
-    "validarNombre() no funciono con un string vacio"
+    "validarUsuario no validó que el nombre no este vacío"
   );
-
   console.assert(
-    validarNombre("111111111111111111111111111111111111111111111111111") ===
+    validarNombre("11111111111111111111111111111111111111111111111111") ===
       "Este campo debe tener menos de 50 caracteres",
     "validarNombre no valido que el nombre sea menor a 50 caracteres"
   );
 
   console.assert(
     validarNombre("Fabricio") === "",
-    "validarnombre falló con un nombre correcto"
+    "Validar nombre falló con un nombre válido"
   );
 }
 
 probarValidarNombre();
 
-function probarValidarCiudad() {
-  console.assert(
-    validarCiudad("Buenos Aires") === "",
-    "validarCiudad no funciono con un nombre de ciudad válido"
-  );
-  console.assert(
-    validarCiudad(" ") === "El campo ciudad no puede estar vacio",
-    "validarCiudad no mostró un error cuando la ciudad es vacía"
-  );
-}
-
-probarValidarCiudad();
-
-function probarValidarDescripcionRegalo() {
-  console.assert(
-    validarDescripcionRegalo("") === "El campo no puede estar vacio",
-    "validarDescripcionRegalo() no funciona si esta vacío"
-  );
-}
+function probarValidarCiudad() {}
